ac0575833eb0d60371a78e1ce9f99ccd4988e9c4
Giorgos <getamvakas@york.citycollege.eu>
2021-12-06 22:02:31 +0200

    Update UrlController

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Url;
 
 class UrlController extends Controller
 {
-    //
+
+    public function shortenURL(Request $request){
+        $baseURL = 'http://127.0.0.1:8000/';
+        $longURL = $request->input('url');
+        $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $shortURL = $baseURL . substr(str_shuffle($chars), 0, 6);
+
+        Url::create(['long_url' => $longURL, 'short_url' => $shortURL]);
+
+        return view('shorten', [
+            'shortURL' => $shortURL,
+
+        ]);
+
+    }
+
+
 }
