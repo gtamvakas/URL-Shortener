86f0fb020438a61cb42342b1b867528588d8ca94
Giorgos <getamvakas@york.citycollege.eu>
2021-12-07 17:14:55 +0200

    Update UrlController.php
    Add a check in redirectToOriginalURL to check whether the URL exists and if it doesn't redirect the user to the home view with an error

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -13,9 +13,9 @@ class UrlController extends Controller
     public function shortenURL(Request $request){
         $baseURL = '127.0.0.1:8000/';
         $longURL = $request->input('url');
+
         $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
         $shortURL = $baseURL . substr(str_shuffle($chars), 0, 6);
-
         Url::create(['long_url' => $longURL, 'short_url' => $shortURL]);
 
         return view('shorten', [
@@ -29,7 +29,9 @@ class UrlController extends Controller
         $baseURL = '127.0.0.1:8000/';
         $shortURL = $request->url_id;
         $originalURL = Url::where('short_url', $baseURL .  $shortURL)->first();
-
+        if($originalURL == null){
+            return redirect('/')->withErrors(['msg' => 'URL does not exist!']);
+        }
        return redirect()->away('https://' .$originalURL->long_url);
     }
 
