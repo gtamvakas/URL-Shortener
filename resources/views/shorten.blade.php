8a01cf3e71da6fb575b3c5a6aab6b4aaf025340d
Giorgos <getamvakas@york.citycollege.eu>
2022-01-02 20:47:43 +0200

    Change CSS in shorten.blade.php

--- resources/views/shorten.blade.php
+++ resources/views/shorten.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <script>
-        new ClipboardJS('.btn')
+        new ClipboardJS('#btn')
         function displayMsg(){
             document.getElementById('copied-msg').hidden = false;
         }
@@ -19,7 +19,7 @@
         <div class="w-full sm:max-w-2xl mt-6 px-10 py-20 bg-white shadow-2xl overflow-hidden sm:rounded-lg">
             <p class="font-bold text-2xl text-center">Your shortened URL</p>
             <input type="text" id="short-url" class="block w-full mt-4" value="{{ $shortURL }}" autofocus><br>
-            <button value="Shorten URL" data-clipboard-target="#short-url" onclick="displayMsg()" class="mt-5 p-2 border border-blue-500  bg-blue-500 text-white rounded-md hover:cursor-pointer">
+            <button id="btn" value="Shorten URL" data-clipboard-target="#short-url" onclick="displayMsg()" class="mt-5 p-2 border border-blue-500  bg-blue-500 text-white rounded-md hover:cursor-pointer">
                 Copy URL
             </button>
             <p id="copied-msg" class="text-green-500" hidden>Copied!</p><br><br><br>
